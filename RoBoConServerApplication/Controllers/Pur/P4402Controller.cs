ecd9151fd38345c5b5b996fa223dc74cc6edd67b
yschoi <dydtn337@naver.com>
2022-11-03 13:25:32 +0900

    2022-11-03  최용수 편리사 원자재 원가표 수정

---
+++ RoBoConServerApplication/Controllers/Pur/P4402Controller.cs
@@ -0,0 +1,155 @@
+﻿using ModelsLibrary.Pur;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using VisualServerApplication.Config;
+
+namespace VisualServerApplication.Controllers.Pur
+{
+    [RoutePrefix("p4402")]
+    public class P4402Controller : ApiController
+    {
+        /// <summary>
+        /// 원자재 원가표 MST - 조회
+        /// </summary>
+        [Route("mst")]
+        [HttpPost]
+        // GET api/<controller>
+        public async Task<IHttpActionResult> GetMstSelect([FromBody] PurVo vo)
+        {
+            try
+            {
+                return Ok<IEnumerable<PurVo>>(Properties.EntityMapper.QueryForList<PurVo>("P4402SelectMstList", vo));
+            }
+            catch (System.Exception eLog)
+            {
+                return Ok<string>(eLog.Message);
+            } 
+        }
+
+        /// <summary>
+        /// 원자재 원가표 DTL - 조회
+        /// </summary>
+        [Route("dtl")]
+        [HttpPost]
+        // GET api/<controller>
+        public async Task<IHttpActionResult> GetDtlSelect([FromBody] PurVo vo)
+        {
+            try
+            {
+                return Ok<PurVo>(Properties.EntityMapper.QueryForObject<PurVo>("P4402SelectDtlList", vo));
+            }
+            catch (System.Exception eLog)
+            {
+                return Ok<string>(eLog.Message);
+            }
+        }
+
+        /// <summary>
+        /// 원자재 원가표 DTL - 추가 및 수정
+        /// </summary>
+        [Route("dtl/i")]
+        [HttpPost]
+        // POST api/<controller>
+        public async Task<IHttpActionResult> GetDtlInsert([FromBody] PurVo[] volist)
+        {
+            try
+            {
+                Properties.EntityMapper.BeginTransaction();
+
+                PurVo vo = new PurVo();
+                vo = volist[0];
+
+                // 마스터에 파일 넣기
+                Properties.EntityMapper.Insert("P4402InsertMst", vo);
+
+                // 디테일 데이터 삭제
+                Properties.EntityMapper.Delete("P4402DeleteDtl", vo);
+                
+                // 디테일 데이터 추가
+                foreach(PurVo _vo in volist)
+                {
+                    Properties.EntityMapper.Insert("P4402InsertDtl", _vo);
+                }
+
+                Properties.EntityMapper.CommitTransaction();
+                return Ok<int>(1);             
+            }
+            catch (System.Exception eLog)
+            {
+                Properties.EntityMapper.RollBackTransaction();
+                return Ok<string>(eLog.Message);
+            }
+        }
+
+        /// <summary>
+        /// 원자재 원가표 DTL 추가 페이지 - 매입처명 조회
+        /// </summary>
+        [Route("dig/co")]
+        [HttpPost]
+        // GET api/<controller>
+        public async Task<IHttpActionResult> CoNmSelect([FromBody] PurVo vo)
+        {
+            try
+            {
+                return Ok<IEnumerable<PurVo>>(Properties.EntityMapper.QueryForList<PurVo>("P4402SelectCoNmList", vo));
+            }
+            catch (System.Exception eLog)
+            {
+                return Ok<string>(eLog.Message);
+            }
+        }
+
+        /// <summary>
+        /// 원자재 원가표 파일 - 조회
+        /// </summary>
+        [Route("file")]
+        [HttpPost]
+        // GET api/<controller>
+        public async Task<IHttpActionResult> GetFileSelect([FromBody] PurVo vo)
+        {
+            try
+            {
+                return Ok<PurVo>(Properties.EntityMapper.QueryForObject<PurVo>("P4402SelectFile", vo));
+            }
+            catch (System.Exception eLog)
+            {
+                return Ok<string>(eLog.Message);
+            }
+        }
+
+
+        
+
+        //// GET api/<controller>
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<controller>
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ VisualErpWpfApp/ViewModel/P4402ViewModel.cs
@@ -0,0 +1,244 @@
+﻿using AquilaErpWpfApp3.Util;
+using AquilaErpWpfApp3.View.PUR.Dialog;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Xpf.Charts;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Spreadsheet;
+using DevExpress.Xpf.WindowsUI;
+using ModelsLibrary.Code;
+using ModelsLibrary.Pur;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace AquilaErpWpfApp3.ViewModel
+{
+    public sealed class P4402ViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        private P4402ExcelDialog excelDialog;
+        private System.IO.Stream streamFile;
+
+        PurVo fileVo = new PurVo();
+
+        public P4402ViewModel()
+        {
+            SYSTEM_CODE_VO();
+        }
+
+        [Command]
+        public async void Refresh()
+        {
+            try
+            {
+                DXSplashScreen.Show<ProgressWindow>();
+
+                using (HttpResponseMessage response = await SystemProperties.PROGRAM_HTTP.PostAsync("p4402/mst", new StringContent(JsonConvert.SerializeObject(new PurVo() { PAY_TP_CD = CO_TP_OBJ.CLSS_CD , CHNL_CD = SystemProperties.USER_VO.CHNL_CD }), System.Text.Encoding.UTF8, "application/json")))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        this.SelectMstList = JsonConvert.DeserializeObject<IEnumerable<PurVo>>(await response.Content.ReadAsStringAsync()).Cast<PurVo>().ToList();
+                    }
+
+                    if (SelectMstList.Count >= 1)
+                    {
+                        //isM_UPDATE = true;
+                        //SelectedMstItem = SelectMstList[0];
+                    }
+                    else
+                    {
+                        //isM_UPDATE = false;
+                    }
+
+                    DXSplashScreen.Close();
+                }
+            }
+            catch (System.Exception eLog)
+            {
+                if (DXSplashScreen.IsActive == true)
+                {
+                    DXSplashScreen.Close();
+                }
+                WinUIMessageBox.Show(eLog.Message, "[" + SystemProperties.PROGRAM_TITLE + "]원자재 원가표", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.None);
+                return;
+            }
+        }
+
+        [Command]
+        public async void NewContact()
+        {
+            try
+            {
+                using (HttpResponseMessage response = await SystemProperties.PROGRAM_HTTP.PostAsync("p4402/file", new StringContent(JsonConvert.SerializeObject(this.SelectedMstItem), System.Text.Encoding.UTF8, "application/json")))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        PurVo vo = new PurVo();
+                        vo = JsonConvert.DeserializeObject<PurVo>(await response.Content.ReadAsStringAsync());
+
+                        if (vo != null)
+                        {
+                            excelDialog = new P4402ExcelDialog(vo);
+                            excelDialog.Title = "원자재 원가표 - 추가";
+                            excelDialog.Owner = Application.Current.MainWindow;
+                            excelDialog.BorderEffect = BorderEffect.Default;
+                            excelDialog.BorderEffectActiveColor = new SolidColorBrush(Color.FromRgb(255, 128, 0));
+                            excelDialog.BorderEffectInactiveColor = new SolidColorBrush(Color.FromRgb(255, 170, 170));
+                            bool isDialog = (bool)excelDialog.ShowDialog();
+                            if (isDialog)
+                            {
+                                Refresh();
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            catch (System.Exception eLog)
+            {
+                WinUIMessageBox.Show(eLog.Message, "[" + SystemProperties.PROGRAM_TITLE + "]원자재 원가표", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.None);
+                return;
+            }
+        }
+
+        [Command]
+        public async void UpdateContact()
+        {
+            try
+            {
+                excelDialog = new P4402ExcelDialog(fileVo);
+                excelDialog.Title = "원자재 원가표 - 수정";
+                excelDialog.Owner = Application.Current.MainWindow;
+                excelDialog.BorderEffect = BorderEffect.Default;
+                excelDialog.BorderEffectActiveColor = new SolidColorBrush(Color.FromRgb(255, 128, 0));
+                excelDialog.BorderEffectInactiveColor = new SolidColorBrush(Color.FromRgb(255, 170, 170));
+                bool isDialog = (bool)excelDialog.ShowDialog();
+                if (isDialog)
+                {
+                    Refresh();
+                }
+            }
+            catch (System.Exception eLog)
+            {
+                WinUIMessageBox.Show(eLog.Message, "[" + SystemProperties.PROGRAM_TITLE + "]원자재 원가표", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.None);
+                return;
+            }
+        }
+
+        public async void SYSTEM_CODE_VO()
+        {
+            using (HttpResponseMessage response = await SystemProperties.PROGRAM_HTTP.GetAsync("S131/dtl/" + Properties.Settings.Default.SettingChnl + "/" + "L-011"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    this.CO_TP_List = JsonConvert.DeserializeObject<IEnumerable<SystemCodeVo>>(await response.Content.ReadAsStringAsync()).Cast<SystemCodeVo>().ToList();
+                    // 업체 구분 리스트 첫 번째 항목 기본값 설정
+                    this.CO_TP_OBJ = this.CO_TP_List[0];
+                }
+            }
+        }
+
+        public async void DetailView()
+        {
+            try
+            {
+                if(this.SelectedMstItem == null)
+                {
+                    isM_UPDATE = false;
+                    return;
+                }
+
+                isM_UPDATE = true;
+
+                using (HttpResponseMessage response = await SystemProperties.PROGRAM_HTTP.PostAsync("p4402/dtl", new StringContent(JsonConvert.SerializeObject(this.SelectedMstItem), System.Text.Encoding.UTF8, "application/json")))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        fileVo = JsonConvert.DeserializeObject<PurVo>(await response.Content.ReadAsStringAsync());
+
+                        if (fileVo != null)
+                        {
+                            this.streamFile = new System.IO.MemoryStream(fileVo.PRC_FILE);
+                            SourceStream = new SpreadsheetDocumentSource(streamFile, DevExpress.Spreadsheet.DocumentFormat.Xlsx);
+                        }
+
+                        //this.spreadsheetControl1.ActiveViewZoom = 85;
+                    }
+                }
+            }
+            catch(System.Exception eLog)
+            {
+                WinUIMessageBox.Show(eLog.Message, "[" + SystemProperties.PROGRAM_TITLE + "]" + "원자재 원가표", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.None, MessageBoxOptions.None);
+                return;
+            }
+
+        }
+        // 업체 구분 리스트
+        private IList<SystemCodeVo> cotplst = new List<SystemCodeVo>();
+        public IList<SystemCodeVo> CO_TP_List
+        {
+            get { return cotplst; }
+            set { SetProperty(ref cotplst, value, () => CO_TP_List); }
+        }
+        // 업체 구분 하나 선택
+        private SystemCodeVo cotpobj = new SystemCodeVo();
+        public SystemCodeVo CO_TP_OBJ
+        {
+            get { return cotpobj; }
+            set { SetProperty(ref cotpobj, value, () => CO_TP_OBJ); }
+        }
+
+
+        //마스터 리스트 조회
+        private IList<PurVo> selectmstlist = new List<PurVo>();
+        public IList<PurVo> SelectMstList
+        {
+            get { return selectmstlist; }
+            set { SetProperty(ref selectmstlist, value, () => SelectMstList); }
+        }
+        // 마스터 하나 선택
+        private PurVo selectmstItm = new PurVo();
+        public PurVo SelectedMstItem
+        {
+            get { return selectmstItm; }
+            set { SetProperty(ref selectmstItm, value, () => SelectedMstItem); DetailView(); }
+        }
+
+        // 
+        private bool ismupdate = false;
+        public bool isM_UPDATE
+        {
+            get { return ismupdate; }
+            set { SetProperty(ref ismupdate, value, () => isM_UPDATE); }
+        }
+
+        SpreadsheetDocumentSource _sourceStream;
+        public SpreadsheetDocumentSource SourceStream
+        {
+            get
+            {
+                return _sourceStream;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    SetProperty(ref _sourceStream, value, () => SourceStream);
+                }
+            }
+        }
+
+
+
+
+    }
+}
